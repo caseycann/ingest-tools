ca7e897d7f84e7b5094563b5109b94bf10656d5e
caseycann <canncasey@gmail.com>
2023-08-22 13:52:02 -0400

    randomStills update

---
+++ randomStills-math.md
@@ -0,0 +1,9 @@
+total Shoots: 
+
+- AllDay: 2 drives x 22 days: 440 images  
+- Small studio: 10 shoots x 3 device: 300 images
+- Classroom rec: 5 shoots x 1 device: 50 images
+- Main Studio: 20 shoots x 4 devices: 800 images
+- Location: 5 shoots x 2 devices: 100 images
+
+All told: 1690 images/month
--- randomStills.js
+++ randomStills.js
@@ -4,8 +4,10 @@ import { spawnSync } from 'child_process';
 import { WebClient } from '@slack/web-api';
 import dotenv from 'dotenv';
 dotenv.config();
+import Airtable from 'airtable';
 
-const slack = new WebClient(process.env.SLACK_TOKEN);
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
+const slack = new WebClient(process.env.SLACK_USER_TOKEN);
 
 
 async function randomStills(shootFolderPath) {
@@ -18,10 +20,10 @@ async function randomStills(shootFolderPath) {
           const filePath = join(cameraPath, file);
           if (statSync(filePath).isFile() && isVideoFile(file)) {
             const duration = getVideoDuration(filePath);
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 1; i++) {
               const randomTime = getRandomTime(duration);
               const timestamp = new Date(randomTime * 1000).toISOString().substr(11, 8);
-              const timestampForFileName = timestamp.replace(/:/g, ""); // Replace colons with underscore
+              const timestampForFileName = timestamp.replace(/:/, ""); // Replace colons with underscore
               const outputFilePath = join(shootFolderPath, `${parse(file).name}_${timestampForFileName}.jpg`);
               extractStill(filePath, randomTime, outputFilePath);
               await sendImageToSlack(outputFilePath, process.env.SLACK_CHANNEL);
@@ -42,6 +44,8 @@ function getVideoDuration(videoFilePath) {
   const ffprobeOutput = spawnSync('ffprobe', [
     '-v', 'quiet', '-print_format', 'json', '-show_entries', 'format=duration', videoFilePath
   ]).stdout.toString();
+  // console.log(ffprobeOutput);
+
   return JSON.parse(ffprobeOutput).format.duration;
 }
 
@@ -50,53 +54,84 @@ function getRandomTime(duration) {
 }
 
 function extractStill(videoFilePath, timestamp, outputFilePath) {
-    const ffmpegProcess = spawnSync('ffmpeg', [
-      '-ss',
-      timestamp,
-      '-i',
-      videoFilePath,
-      '-vframes',
-      '1',
-      '-q:v', 
-      '15', // Adjust this value to change the quality (and therefore size) of the output image
-      outputFilePath
-    ]);
-  
-    if (ffmpegProcess.error) {
-      console.error(`Error executing ffmpeg: ${ffmpegProcess.error.message}`);
-    }
+  const ffmpegProcess = spawnSync('ffmpeg', [
+    '-ss',
+    timestamp,
+    '-i',
+    videoFilePath,
+    '-vframes',
+    '1',
+    '-vf', 
+    'scale=1280:-1',  // This scales the image
+    '-q:v', 
+    '17', // Adjust this value to change the quality (and therefore size) of the output image
+    outputFilePath
+  ]);
+
+  if (ffmpegProcess.error) {
+    console.error(`Error executing ffmpeg: ${ffmpegProcess.error.message}`);
   }
-  
+}
 
   
-  
+async function sendImageToSlack(filePath, channel) {
+  try {
+      const result = await slack.files.uploadV2({
+          file: createReadStream(filePath),
+          filename: basename(filePath),
+          channel_id: channel,
+          initial_comment: `${basename(filePath)}`
+      });
 
-  async function sendImageToSlack(filePath, channel) {
-    try {
-      const result = await slack.files.upload({
-        file: createReadStream(filePath),
-        filename: basename(filePath),
-        channels: channel,
-        initial_comment: `Here's an image extracted from a video: ${basename(filePath)}`
-      });
-  
-      console.log('File sent: ', result.file.id);
-  
-      // Enable public URL for the file
-      const publicUrlResult = await slack.files.sharedPublicURL({
-        token: process.env.SLACK_USER_TOKEN,
-        file: result.file.id
-      });
-  
-      console.log('Public URL: ', publicUrlResult.file.permalink_public);
-  
-    } catch (error) {
+      // Check if the returned result has the expected structure.
+      if (result && result.files && result.files[0] && result.files[0].file) {
+          const file = result.files[0].file;
+          console.log('File sent: ', file.id);
+
+          function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+          }
+
+          await sleep(3000); // wait for 3 seconds
+
+          const filePathURL = makeSlackImageURL(file.permalink, file.permalink_public);
+          console.log('Public URL: ', filePathURL);
+          await sendImageToAirtable(basename(filePath), filePathURL);
+
+          // Return the file information for further processing
+          return file;
+      }
+  } catch (error) {
       console.error('Error sending file: ', error);
-    }
   }
-  
-  
-  
-  
+}
 
+  
+  
+async function sendImageToAirtable(filename, publicUrl) {
+  const table = base(process.env.RANDOMSTILLS_TABLE);  
+  try {
+      const record = await table.create({
+          'name': filename,
+          'public_url': publicUrl,
+          // Add more fields if needed
+      });
+
+      console.log('Record added to Airtable:', record.id);
+      return record;
+  } catch (error) {
+      console.error('Error sending image to Airtable:', error);
+  }
+}
+
+function makeSlackImageURL (permalink, permalink_public) {
+  let secrets = (permalink_public.split("slack-files.com/")[1]).split("-")
+  let suffix = permalink.split("/")[(permalink.split("/").length - 1)]
+  let filePath = `https://files.slack.com/files-pri/${secrets[0]}-${secrets[1]}/${suffix}?pub_secret=${secrets[2]}`
+  return filePath
+}
+
+
+  
 export { randomStills };
+
