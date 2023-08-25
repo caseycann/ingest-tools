5d1a7095458e0a4d2e65decdcadfceab2b085999
caseycann <canncasey@gmail.com>
2023-08-25 12:56:45 -0400

    randomStills

--- randomStills.js
+++ randomStills.js
@@ -1,39 +1,100 @@
 import { readdirSync, statSync, createReadStream } from 'fs';
 import { join, parse, basename } from 'path';
-import { spawnSync } from 'child_process';
+import { spawnSync, execSync } from 'child_process';
 import { WebClient } from '@slack/web-api';
 import dotenv from 'dotenv';
 dotenv.config();
 import Airtable from 'airtable';
+import fs from 'fs';
+
 
 const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
 const slack = new WebClient(process.env.SLACK_USER_TOKEN);
-
+  
 
 async function randomStills(shootFolderPath) {
-    const cameras = readdirSync(shootFolderPath);
-    for (const camera of cameras) {
+  const cameras = readdirSync(shootFolderPath);
+  for (const camera of cameras) {
       const cameraPath = join(shootFolderPath, camera);
       if (statSync(cameraPath).isDirectory()) {
-        const files = readdirSync(cameraPath);
-        for (const file of files) {
-          const filePath = join(cameraPath, file);
-          if (statSync(filePath).isFile() && isVideoFile(file)) {
-            const duration = getVideoDuration(filePath);
-            for (let i = 0; i < 1; i++) {
-              const randomTime = getRandomTime(duration);
-              const timestamp = new Date(randomTime * 1000).toISOString().substr(11, 8);
-              const timestampForFileName = timestamp.replace(/:/, ""); // Replace colons with underscore
-              const outputFilePath = join(shootFolderPath, `${parse(file).name}_${timestampForFileName}.jpg`);
-              extractStill(filePath, randomTime, outputFilePath);
-              await sendImageToSlack(outputFilePath, process.env.SLACK_CHANNEL);
-            }
+          const files = readdirSync(cameraPath);
+          for (const file of files) {
+              const filePath = join(cameraPath, file);
+              if (statSync(filePath).isFile() && isVideoFile(file)) {
+                  const duration = getVideoDuration(filePath);
+                  for (let i = 0; i < 5; i++) {
+                      const randomTime = getRandomTime(duration);
+                      const startTimecode = getStartTimecode(filePath);
+                      const outputFilePath = join(shootFolderPath, `${parse(file).name}_${startTimecode.replace(/:/g, "")}.jpg`);
+                      extractStill(filePath, randomTime, outputFilePath);
+                      await sendImageToSlack(outputFilePath, process.env.SLACK_CHANNEL);
+                      // Send to Airtable if you have that function...
+                      
+                      // Now, delete the local image
+                      try {
+                          fs.unlinkSync(outputFilePath);
+                          console.log(`Successfully deleted local image: ${outputFilePath}`);
+                      } catch (error) {
+                          console.error(`Error deleting local image: ${outputFilePath}`, error);
+                      }
+                  }
+              }
           }
-        }
       }
-    }
   }
+}
+
+
+function getStartTimecode(videoFilePath) {
+  try {
+    const ffprobeOutput = execSync(
+      `ffprobe -v error -select_streams v:0 -print_format json -show_entries stream_tags=timecode "${videoFilePath}"`,
+      { encoding: 'utf8' }
+    );
+
+    const jsonData = JSON.parse(ffprobeOutput);
+    let rawTimecode = jsonData.streams[0].tags.timecode;
+
+    // Format timecode to ensure 8 digits
+    return formatTimecode(rawTimecode);
+
+  } catch (error) {
+    console.error('Error retrieving timecode:', error);
+    return "00:00:00:00";
+  }
+}
+
+function timecodeToSeconds(timecode) {
+  console.log("Converting timecode:", timecode);  // Debugging line
+  const [hours, minutes, seconds] = timecode.split(":").map(Number);
+  return hours * 3600 + minutes * 60 + seconds;
+}
+
+function addTimecodes(startTimecode, secondsOffset) {
+  console.log("Adding timecode:", startTimecode, "with offset:", secondsOffset);  // Debugging line
+  const startInSeconds = timecodeToSeconds(startTimecode);
+  const resultInSeconds = startInSeconds + secondsOffset;
+  return secondsToTimecode(resultInSeconds);
+}
+
+function secondsToTimecode(totalSeconds) {
+  const hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
   
+  // Using `String.prototype.padStart()` to ensure each unit is two digits
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toFixed(0).toString().padStart(2, '0')}`;
+}
+
+function formatTimecode(timecode) {
+  const parts = timecode.split(':');
+  while (parts.length < 4) {
+      parts.unshift('00');
+  }
+  return parts.join(':');
+}
+
 
 function isVideoFile(filename) {
   const videoExtensions = ['.mp4', '.mov', '.avi', '.mkv', '.m4v'];
@@ -62,9 +123,9 @@ function extractStill(videoFilePath, timestamp, outputFilePath) {
     '-vframes',
     '1',
     '-vf', 
-    'scale=1280:-1',  // This scales the image
+    'scale=1920:-1',  // This scales the image
     '-q:v', 
-    '17', // Adjust this value to change the quality (and therefore size) of the output image
+    '2', // Adjust this value to change the quality (and therefore size) of the output image
     outputFilePath
   ]);
 
@@ -76,8 +137,9 @@ function extractStill(videoFilePath, timestamp, outputFilePath) {
   
 async function sendImageToSlack(filePath, channel) {
   try {
+      const imageData = fs.readFileSync(filePath);
       const result = await slack.files.uploadV2({
-          file: createReadStream(filePath),
+          file: imageData,
           filename: basename(filePath),
           channel_id: channel,
           initial_comment: `${basename(filePath)}`
@@ -94,7 +156,17 @@ async function sendImageToSlack(filePath, channel) {
 
           await sleep(3000); // wait for 3 seconds
 
-          const filePathURL = makeSlackImageURL(file.permalink, file.permalink_public);
+          // Create a public shared URL for the file
+          const sharedPublicURLResult = await slack.files.sharedPublicURL({
+              file: file.id
+          });
+
+          if (!sharedPublicURLResult.ok) {
+              console.error('Error making file public: ', sharedPublicURLResult.error);
+              return;
+          }
+
+          const filePathURL = makeSlackImageURL(file.permalink, sharedPublicURLResult.file.permalink_public);
           console.log('Public URL: ', filePathURL);
           await sendImageToAirtable(basename(filePath), filePathURL);
 
@@ -105,6 +177,7 @@ async function sendImageToSlack(filePath, channel) {
       console.error('Error sending file: ', error);
   }
 }
+
 
   
   
